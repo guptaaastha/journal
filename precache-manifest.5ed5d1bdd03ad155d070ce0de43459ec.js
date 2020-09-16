f65bdaa5429c2c73a49d9f4d01e28a50624173c2
Aastha Gupta <18bit102@ietdavv.edu.in>
2020-09-16 19:12:04 +0530

    Updates

---
+++ precache-manifest.5ed5d1bdd03ad155d070ce0de43459ec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "090e9e378d77c6e200f8d3a5db328fa7",
+    "url": "/journal/index.html"
+  },
+  {
+    "revision": "57f85b3955702405bf66",
+    "url": "/journal/static/css/main.cf1adfbd.chunk.css"
+  },
+  {
+    "revision": "a06a8ab95502c2c3c248",
+    "url": "/journal/static/js/2.1666a5c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/journal/static/js/2.1666a5c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57f85b3955702405bf66",
+    "url": "/journal/static/js/main.c3da40ce.chunk.js"
+  },
+  {
+    "revision": "190aaad2d9ab9e8a1d2e",
+    "url": "/journal/static/js/runtime-main.bc559b61.js"
+  }
+]);
