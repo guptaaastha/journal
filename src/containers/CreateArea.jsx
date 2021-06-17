450605f3ce94d2c50ea32f9beed4aa354c5e1c56
Aastha Gupta <18bit102@ietdavv.edu.in>
2021-06-17 15:38:55 +0530

    Add color choice functionality

--- src/containers/CreateArea.jsx
+++ src/containers/CreateArea.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import classes from "./CreateArea.module.css";
 import { ToastProvider, useToasts } from "react-toast-notifications";
+import { BlockPicker } from "react-color";
 
 function CreateArea(props) {
   const [entry, setEntry] = useState({ title: "", content: "" });
   const { addToast } = useToasts();
+  const [color, setColor] = useState();
+  const [pickerVisible, setPickerVisible] = useState(false);
 
   function handleChange(event) {
     const { name, value } = event.target;
@@ -16,7 +19,7 @@ function CreateArea(props) {
     });
   }
 
-  function submitNote(event) {
+  function submitNote() {
     if (entry.content === "" && entry.title === "")
       addToast("Oops! the entry neither has a title nor any content", {
         appearance: "error",
@@ -30,31 +33,71 @@ function CreateArea(props) {
       title: "",
       content: "",
     });
-    event.preventDefault();
   }
 
   return (
     <ToastProvider>
       <div className={classes.CreateArea}>
-        <div className={classes.topdiv}>
-          <form autoComplete="off">
-            <input
-              name="title"
-              value={entry.title}
-              onChange={handleChange}
-              placeholder="wanna put a title?"
-            />
-            <div></div>
-            <textarea
-              name="content"
-              value={entry.content}
-              onChange={handleChange}
-              placeholder="pour your heart out"
-            />
-            <button type="button" onClick={submitNote}>
-              add
+        <div
+          className={classes.topdiv}
+          style={{ backgroundColor: color ? color.hex : "#ffffff" }}
+        >
+          <input
+            name="title"
+            value={entry.title}
+            onChange={handleChange}
+            placeholder="wanna put a title?"
+            style={{ backgroundColor: color ? color.hex : "#ffffff" }}
+          />
+          <textarea
+            name="content"
+            value={entry.content}
+            onChange={handleChange}
+            placeholder="pour your heart out"
+            style={{
+              backgroundColor: color ? color.hex : "#ffffff",
+              marginTop: 25,
+            }}
+          />
+          {pickerVisible && (
+            <div
+              style={{
+                position: "absolute",
+              }}
+            >
+              <BlockPicker
+                color={color}
+                onChangeComplete={(c) => {
+                  setColor(c);
+                  setPickerVisible(false);
+                }}
+              />
+            </div>
+          )}
+          <div
+            style={{
+              width: "100%",
+              display: "flex",
+              justifyContent: "space-between",
+              padding: 0,
+              margin: 0,
+            }}
+          >
+            <p
+              style={{ margin: 0, padding: 0, fontSize: 20, cursor: "pointer" }}
+              onClick={() => setPickerVisible(!pickerVisible)}
+            >
+              ✏️
+            </p>
+            <button
+              onClick={() => {
+                submitNote();
+                setPickerVisible(false);
+              }}
+            >
+              +
             </button>
-          </form>
+          </div>
         </div>
         <hr />
       </div>
